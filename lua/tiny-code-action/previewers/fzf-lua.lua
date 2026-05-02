20e1d6789a4b7a26ef7d4ed4bcf64442c95ced7b
madkuntilanak <fhzcloth@gmail.com>
2026-05-03 01:29:09 +0700

    fix: error previewers

--- lua/tiny-code-action/previewers/fzf-lua.lua
+++ lua/tiny-code-action/previewers/fzf-lua.lua
@@ -36,14 +36,18 @@ local function preview_cmd(opts)
   function CodeActionPreviewerCmd:cmdline(o)
     o = o or {}
     return shell.stringify_cmd(function(entry_str)
+      local text
       if type(entry_str) ~= "table" then
-        local fallback_cmd = utils.create_echo_command("No preview available for this action")
-        return table.concat(fallback_cmd, " ")
+        text = "No preview available for this action"
+      else
+        local preview_content = extract_preview_data(entry_str[1], opts)
+        text = table.concat(preview_content, "\n")
       end
 
-      local preview_content = extract_preview_data(entry_str[1], opts)
-      local text = table.concat(preview_content, "\n")
-      local cmd = utils.create_echo_command(text)
+      local cmd = utils.create_echo_command(text .. "\n")
+      for i = 1, #cmd do
+        cmd[i] = vim.fn.shellescape(cmd[i])
+      end
       return table.concat(cmd, " ")
     end, {}, "{}")
   end
